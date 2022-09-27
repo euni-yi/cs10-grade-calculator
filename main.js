732592d1f6d366a6ddef9b4a7511dd74ba662608
euni-yi <ejky53@gmail.com>
2022-09-27 11:23:12 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// JavaScript Code
+// CS-10 Grade Calculator
+
+// Input
+let first= +prompt("Enter yoru Client Side Scripting 1 Grade:");
+let second= +prompt("Enter yoru Structured Programming 1 Grade:");
+let third= +prompt("Enter your Structure Programming 2 Grade:");
+let fourth= +prompt("Enter your Client Side Scripting 2 Grade:");
+let fifth= +prompt("Enter your Project A Grade:");
+
+// Process
+let total = ([first + second + third + fourth + fifth]/100 );
+
+// Output
+alert(`Your Final CS10 Grade is: ${first} + ${second} + ${third} + ${fourth} + ${fifth}: = ${total}% `)

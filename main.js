667ca71ccb29a7773706aa6c9989e5998defe987
euni-yi <ejky53@gmail.com>
2022-09-28 10:13:49 -0600

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let fourth= +prompt("Enter your Client Side Scripting 2 Grade:");
 let fifth= +prompt("Enter your Project A Grade:");
 
 // Process
-let total = ([first + second + third + fourth + fifth]/100 );
+let total = (first + second + third + fourth + fifth) / 5 ;
 
 // Output
 alert(`Your Final CS10 Grade is: ${first} + ${second} + ${third} + ${fourth} + ${fifth}: = ${total}% `)

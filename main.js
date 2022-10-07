c62e0a56d0d1f1de995647298d03df8c28713a0d
euni-yi <ejky53@gmail.com>
2022-10-07 10:40:51 -0600

    finish

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 // CS-10 Grade Calculator
 
+// Button Event Listener
+
+document.getElementById("btn").addEventListener("click", btnClicked)
+
+function btnClicked(){
+
 // Input
-let first= +prompt("Enter yoru Client Side Scripting 1 Grade:");
-let second= +prompt("Enter yoru Structured Programming 1 Grade:");
-let third= +prompt("Enter your Structure Programming 2 Grade:");
-let fourth= +prompt("Enter your Client Side Scripting 2 Grade:");
-let fifth= +prompt("Enter your Project A Grade:");
+    let first= +document.getElementById("1").value;
+    let second= +document.getElementById("2").value;
+    let third= +document.getElementById("3").value;
+    let fourth= +document.getElementById("4").value;
+    let fifth= +document.getElementById("5").value;
 
 // Process
-let total = (first + second + third + fourth + fifth) / 5 ;
+    let total = (first + second + third + fourth + fifth) / 5 ;
 
 // Output
-alert(`Your Final CS10 Grade is: ${first} + ${second} + ${third} + ${fourth} + ${fifth}: = ${total}% `)
+    document.getElementById("finalgrade").innerHTML = total
+
+}
+
